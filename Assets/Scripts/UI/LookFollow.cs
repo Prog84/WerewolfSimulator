54349cf04cfd8953271c8bc30504add3f7b25ea1
Kalad132 <67950598+Kalad132@users.noreply.github.com>
2021-04-12 18:13:55 +0300

    Camera and Outlines

---
+++ Assets/Scripts/UI/LookFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private Vector3 _levelDirection;
+    [SerializeField] private float _distanse;
+    [SerializeField] private PlayerInput _input;
+
+    void Update()
+    {
+        transform.position = Vector3.Project(_player.position + (_input.Direction + _player.transform.forward).normalized * _distanse, _levelDirection + _player.position);
+    }
+}
